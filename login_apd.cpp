0f6dfe746a80a5ade543876b3e6e76393abc5c5b
Sulthon Mutaqin <33215222+sulthonmutaqin36@users.noreply.github.com>
2019-10-18 14:24:38 +0800

    Update login_apd.cpp

--- login_apd.cpp
+++ login_apd.cpp
@@ -2,21 +2,20 @@
 using namespace std;
 
 int main(){
-		string user, pass;
-		
-		cout << "Login\n\n";
-		login:
-		cout << "Username    = ";
-		cin >> user;
-		cout << "Password    = "
-		cin >> pass;
-		
-		
-		if(user=="admin"&&pass=="admin123"){
-			cout << "Login berhasil";
-		} else {
-			cout << "Password atau username salah\n";
-			goto login;
-		}
-		return 0;
+	string user, pass;
+	
+	cout << "Login\n\n";
+	login:
+	cout << "Username    = ";
+	cin >> user;
+	cout << "Password    = "
+	cin >> pass;	
+	
+	if(user=="admin"&&pass=="admin123"){
+		cout << "Login berhasil";
+	} else {
+		cout << "Password atau username salah\n";
+		goto login;
+	}
+	return 0;
 }
